fd73429f7f259a6b7a829aa662af06700b41c6ff
Thalles <rangelthr@gmail.com>
2022-11-16 19:04:48 -0300

    Meee

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import Logo from './../../img/logo.jpg';
 const Header = () => {
   return (
     <div className="text-center">
-      <img className="logo img-fluid" src={Logo}/>
+      <img className="logo img-fluid" src={ Logo } alt="Me"/>
     </div>
   )
 }
