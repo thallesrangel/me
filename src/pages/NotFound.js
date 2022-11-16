9a13c82fa1f45aed226d9a67a6e091cdbfc0ac63
Thalles <rangelthr@gmail.com>
2022-11-16 19:35:30 -0300

    404 PAGE

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import { Container, Row, Col} from 'react-bootstrap';
 import {Link} from 'react-router-dom'
+import Logo from '../img/404.svg';
 
 function NotFound() {
    return (
+    <Container className="center">
      <div className="notFound">
-       <h1>404- Page not Found</h1>
+      <img className="img-fluid" src={ Logo } alt="404"/>
+       <h1>Page not Found</h1>
        
-       <Link to="/">Ir para página inicial</Link>
+       <Link className="btn btn-default" to="/">Home</Link>
      </div>
+     </Container>
  )
 }
 export default NotFound
